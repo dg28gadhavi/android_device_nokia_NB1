5cee44fa372e696690565dee8250a109aa28c86b
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2018-07-25 03:20:25 +0530

    Configure Time Zone data for Recovery

--- omni_NB1.mk
+++ omni_NB1.mk
@@ -87,6 +87,19 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     charger
 
+# Define time zone data path
+ifneq ($(wildcard bionic/libc/zoneinfo),)
+    TZDATAPATH := bionic/libc/zoneinfo
+else ifneq ($(wildcard system/timezone),)
+    TZDATAPATH := system/timezone/output_data/iana
+endif
+
+# Time Zone data for Recovery
+ifdef TZDATAPATH
+PRODUCT_COPY_FILES += \
+$(TZDATAPATH)/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+endif
+
 PRODUCT_DEVICE := NB1
 PRODUCT_NAME := omni_NB1
 PRODUCT_BRAND := Nokia
